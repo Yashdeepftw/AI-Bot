1538e0bec412bccba31cf0a13ec6485ff30048b6
Yashdeepftw <yashdeepchauhan295@gmail.com>
2026-04-06 00:52:30 +0530

    added the api logic for communication with backend

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router'
-import Login from './features/auth/components/pages/Login'
-import Register from './features/auth/components/pages/Register'
+import Login from './features/auth/pages/Login'
+import Register from './features/auth/pages/Register'
 
 export const router = createBrowserRouter([
     {
