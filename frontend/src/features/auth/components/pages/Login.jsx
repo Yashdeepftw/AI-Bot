1ed0dc5be31d40f33aca2cb7c9b79c56836f8683
Yashdeepftw <yashdeepchauhan295@gmail.com>
2026-04-06 00:20:01 +0530

    added the final ui code for register and login page

--- frontend/src/features/auth/components/pages/Login.jsx
+++ frontend/src/features/auth/components/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import '../../form.auth.scss'
+import { Link } from 'react-router';
 
 const Login = () => {
+    const handelSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <>Login </>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form action="" onSubmit={handelSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" placeholder="Enter email address" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder="Enter Password"k />
+                    </div>
+
+                    <button className="button primary-button">
+                        Login
+                    </button>
+
+                </form>
+
+                <p>Don't Have an Account? <Link to={'/register'}>Register</Link></p>
+
+            </div>
+        </main>
     )
 }
 export default Login
--- frontend/src/features/auth/components/pages/Register.jsx
+++ frontend/src/features/auth/components/pages/Register.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import '../../form.auth.scss'
+import { useNavigate, Link } from "react-router";
 
 const Register = () => {
+
+    const navigate = useNavigate();
+     
+    const handelSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
-        <>Register </>
+        <main>
+            <div className="form-container">
+                <h1>Register</h1>
+                <form action="" onSubmit={handelSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" placeholder="Enter email address"/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="name">Username</label>
+                        <input type="text" name="username" placeholder="Enter Username"/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder="Enter Password"/>
+                    </div>
+
+                    <button className="button primary-button">
+                        Register
+                    </button>
+
+                </form>
+                <p>Already Have an Account? <Link to={'/login'}>Login</Link></p>
+            </div>
+        </main>
     )
 }
 
